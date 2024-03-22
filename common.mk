7c1f50c9d6b52aa9f99d11381e632f3c8c9eebd5
R0rt1z2 <me@r0rt1z2.com>
2024-03-22 13:39:13 +0100

    hi3660-common: Build perf libraries from source
    Change-Id: Ife9966d8c42d2625857b450e919db58690c71f53

--- common.mk
+++ common.mk
@@ -235,6 +235,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
 
+PRODUCT_PACKAGES += \
+    vendor.huawei.hardware.perfgenius@2.0.vendor
+
+PRODUCT_PACKAGES += \
+    libiawareperf_client \
+    libperfgenius_vendor_client
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
